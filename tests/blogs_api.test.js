e568b708575ef07e52a6a648aa16238497dbe24c
Maria Villascusa <meri.villa14@gmail.com>
2021-08-24 16:26:32 +0200

    4.11 blogs tests step4

--- tests/blogs_api.test.js
+++ tests/blogs_api.test.js
@@ -61,6 +61,20 @@ test('create a blog', async () => {
     expect(contents).toContain('Post blog')
 })
 
+test('blog without likes it means it have 0 likes', async () => {
+    const newBlog = {
+        title: 'No likes blog',
+        author: 'Toni',
+        url: 'protiren'
+    }
+    await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+    const response = await api.get('/api/blogs')
+    expect(response.body).toHaveLength(initialBlogs.length + 1)
+})
+
 afterAll(() => {
     mongoose.connection.close()
 })
